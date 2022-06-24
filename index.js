b91cc23580f642f18f7dd483f9137a57d7edc195
Mark <markotweyo@gmail.com>
2022-06-24 21:08:11 +0300

    Add pop window

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Mobile-navigation-menu
 const menu = document.querySelector('.menu');
 const menuItems = document.querySelectorAll('.menuItem');
 const hamburger = document.querySelector('.hamburger');
@@ -5,17 +6,6 @@ const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
 
 const toggleMenu = () => {
-<<<<<<< HEAD
-    if (menu.classList.contains('showMenu')) {
-        menu.classList.remove('showMenu');
-        closeIcon.style.display = 'none';
-        menuIcon.style.display = 'block';
-    } else {
-        menu.classList.add('showMenu');
-        closeIcon.style.display = 'block';
-        menuIcon.style.display = 'none';
-    }
-=======
   if (menu.classList.contains('showMenu')) {
     menu.classList.remove('showMenu');
     closeIcon.style.display = 'none';
@@ -25,19 +15,12 @@ const toggleMenu = () => {
     closeIcon.style.display = 'block';
     menuIcon.style.display = 'none';
   }
->>>>>>> e169972f4a9d07f9fde13d70fbfb373a9afb0725
 };
 
 hamburger.addEventListener('click', toggleMenu);
 
 menuItems.forEach(
-<<<<<<< HEAD
-    (menuItem) => {
-        menuItem.addEventListener('click', toggleMenu);
-    }
-=======
   (menuItem) => {
     menuItem.addEventListener('click', toggleMenu);
   },
->>>>>>> e169972f4a9d07f9fde13d70fbfb373a9afb0725
 );
